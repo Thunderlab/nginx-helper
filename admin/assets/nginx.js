a9ab2e22189f706697714d484ad986141668567b
Daniel Martines <daniel.martines@me.com>
2014-12-18 00:18:03 -0200

    Update nginx.js

--- admin/assets/nginx.js
+++ admin/assets/nginx.js
@@ -1,11 +1,4 @@
 jQuery(document).ready(function() {
-    var news_section = jQuery('#latest_news');
-    if (news_section.length > 0) {
-        jQuery.get(news_url, function(data) {
-            news_section.find('.inside').html(data);
-        });
-    }
-    
     /**
      * Show OR Hide options on option checkbox
      * @param {type} selector Selector of Checkbox and PostBox
